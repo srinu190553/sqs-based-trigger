cd73d1eacf28ac7747bf3388a0240c674a26f14c
srinu190553 <143709434+srinu190553@users.noreply.github.com>
2024-06-14 12:15:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,6 +19,27 @@ resource "aws_dynamodb_table" "table" {
 resource "aws_ecs_cluster" "cluster" {
   name = "my-cluster"
 }
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs_task_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
 
 # Create ECS Task Definition
 resource "aws_ecs_task_definition" "task" {
@@ -27,6 +48,7 @@ resource "aws_ecs_task_definition" "task" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([{
     name  = "my-container"
