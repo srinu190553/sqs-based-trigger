6ca70d36144b4398d8298461feaf8258bcb4dba2
srinu190553 <143709434+srinu190553@users.noreply.github.com>
2024-06-15 12:37:43 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "ecs_cluster_name" {
+  type    = string
+  default = "my-cluster"  # Replace with your ECS cluster name or provide as input
+}
+
+variable "ecs_service_name" {
+  type    = string
+  default = "my-service"  # Replace with your ECS service name or provide as input
+}
