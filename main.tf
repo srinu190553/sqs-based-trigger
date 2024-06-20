fcd8cf788420d049517a6d364520dbeb4a4498f2
srinu190553 <143709434+srinu190553@users.noreply.github.com>
2024-06-20 11:08:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,6 @@
-
-# Create SQS Queue
-resource "aws_sqs_queue" "queue" {
-  name = "my-sqs-queue"
-}
-
-# Create DynamoDB Table
-resource "aws_dynamodb_table" "table" {
-  name           = "my-dynamodb-table"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "id"
-  attribute {
-    name = "id"
-    type = "S"
-  }
-}
-
 # Create ECS Cluster
 resource "aws_ecs_cluster" "cluster" {
-  name = "my-cluster"
+  name = "td-sqs-trigger"
 }
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "ecs_task_execution_role"
@@ -41,44 +24,9 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-# Create ECS Task Definition
-resource "aws_ecs_task_definition" "task" {
-  family                   = "my-task-family"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  task_role_arn            = aws_iam_role.ecs_task_execution_role.arn
-
-  container_definitions = jsonencode([{
-    name  = "my-container"
-    image = "933085737869.dkr.ecr.us-east-1.amazonaws.com/my-sqs-dynamodb-app:latest"
-    essential = true
-    environment = [
-      {
-        name = "SQS_QUEUE_URL"
-        value = aws_sqs_queue.queue.id
-      },
-      {
-        name = "DYNAMODB_TABLE_NAME"
-        value = aws_dynamodb_table.table.name
-      }
-    ]
-    logConfiguration = {
-      logDriver = "awslogs"
-      options = {
-        "awslogs-group"        = "/ecs/my-task"
-        "awslogs-region"       = "us-east-1"
-        "awslogs-stream-prefix" = "ecs"
-      }
-    }
-  }])
-}
-
 # Create ECS Service
 resource "aws_ecs_service" "service" {
-  name            = "my-service"
+  name            = "td-sqs-trigger"
   cluster         = aws_ecs_cluster.cluster.id
   task_definition = aws_ecs_task_definition.task.arn
   desired_count   = 1
@@ -135,7 +83,7 @@ resource "aws_appautoscaling_policy" "scale_in_policy" {
 
   step_scaling_policy_configuration {
     adjustment_type         = "ChangeInCapacity"
-    cooldown                = 300
+    cooldown                = 200
     metric_aggregation_type = "Average"
 
     step_adjustment {
@@ -168,83 +116,11 @@ resource "aws_cloudwatch_metric_alarm" "sqs_backlog_per_task_alarm" {
 
 resource "aws_appautoscaling_target" "ecs_service" {
   max_capacity       = 10
-  min_capacity       = 1
+  min_capacity       = 0
   resource_id        = "service/${aws_ecs_cluster.cluster.name}/${aws_ecs_service.service.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
-}
-
-resource "aws_appautoscaling_policy" "ecs_service_target_tracking" {
-  name               = "ecs-service-target-tracking"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.ecs_service.resource_id
-  scalable_dimension = aws_appautoscaling_target.ecs_service.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.ecs_service.service_namespace
-
-  target_tracking_scaling_policy_configuration {
-    target_value = 10.0
-
-    customized_metric_specification {
-      metrics {
-        label = "Get the queue size (the number of messages waiting to be processed)"
-        id    = "m1"
-
-        metric_stat {
-          metric {
-            metric_name = "ApproximateNumberOfMessagesVisible"
-            namespace   = "AWS/SQS"
-
-            dimensions {
-              name  = "QueueName"
-              value = aws_sqs_queue.queue.name
-            }
-          }
-
-          stat = "Sum"
-        }
-
-        return_data = false
-      }
-
-      metrics {
-        label = "Get the ECS running task count (the number of currently running tasks)"
-        id    = "m2"
-
-        metric_stat {
-          metric {
-            metric_name = "RunningTaskCount"
-            namespace   = "ECS/ContainerInsights"
-
-            dimensions {
-              name  = "ClusterName"
-              value = aws_ecs_cluster.cluster.name
-            }
-
-            dimensions {
-              name  = "ServiceName"
-              value = aws_ecs_service.service.name
-            }
-          }
-
-          stat = "Average"
-        }
-
-        return_data = false
-      }
-
-      metrics {
-        label       = "Calculate the backlog per instance"
-        id          = "e1"
-        expression  = "m1 / m2"
-        return_data = true
-      }
-    }
-
-    scale_in_cooldown  = 60
-    scale_out_cooldown = 60
-  }
-}
-  
+}  
 
 resource "aws_iam_role" "ecs_autoscaling_role" {
   name = "ecs-autoscaling-role"
@@ -266,20 +142,5 @@ resource "aws_iam_role" "ecs_autoscaling_role" {
 resource "aws_iam_role_policy_attachment" "ecs_autoscaling_policy" {
   role       = aws_iam_role.ecs_autoscaling_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceAutoscaleRole"
-}
-
-
-# Output the SQS Queue URL
-output "sqs_queue_url" {
-  value = aws_sqs_queue.queue.id
-}
-
-# Output the DynamoDB Table Name
-output "dynamodb_table_name" {
-  value = aws_dynamodb_table.table.name
-}
-
-output "ecs_task_definition_arn" {
-  value = aws_ecs_task_definition.task.arn
 }
 
