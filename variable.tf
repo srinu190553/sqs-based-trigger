4e9008f62c58b65288d8ca0de1bb6e90b9ea035e
srinu190553 <143709434+srinu190553@users.noreply.github.com>
2024-06-21 10:31:11 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,9 +1,9 @@
 variable "ecs_cluster_name" {
   type    = string
-  default = "td-sqs-trigger"  # Replace with your ECS cluster name or provide as input
+  default = "cluster_qs"  # Replace with your ECS cluster name or provide as input
 }
 
 variable "ecs_service_name" {
   type    = string
-  default = "td-sqs-trigger"  # Replace with your ECS service name or provide as input
+  default = "service_qs"  # Replace with your ECS service name or provide as input
 }
